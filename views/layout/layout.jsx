ebba2591bca1fd803fae4028961613439eab3689
valenlyn <valenlynchua@gmail.com>
2019-04-23 01:17:23 +0800

    Fixed mobile view for main / cards page

--- views/layout/layout.jsx
+++ views/layout/layout.jsx
@@ -10,7 +10,8 @@ class Layout extends React.Component {
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
                 <link href="https://fonts.googleapis.com/css?family=Chivo:300,400|Prata|Sarala" rel="stylesheet"/>
                 <link rel="stylesheet" href="/style.css"/>
-                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
 
             </head>
             <body>
